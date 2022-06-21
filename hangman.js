f22b2dc803b7d6539da0716591935b51ba6d18b8
DominikKoltai <106646786+DominikKoltai@users.noreply.github.com>
2022-06-21 15:15:52 +0200

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -3,12 +3,7 @@ const countriesCapitals = require('./countries-and-capitals');
 const capitals = countriesCapitals.map(e => e.city);
 const countries = countriesCapitals.map(e => e.country);
 
-
-function play(word, lives) {
-    word = capitals[Math.floor(Math.random() * capitals.length)];
-    return word;
-}
-play();
+word = capitals[Math.floor(Math.random() * capitals.length)];
 let answerArray = [];
 for (let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
@@ -18,7 +13,7 @@ let remaininLetters = word.length;
 
 while (remaininLetters > 0) {
     console.log(answerArray.join(" "));
-    let guess = prompt("Guess a letter!");
+    let guess = prompt("Guess a letter: ");
     if (guess == null) {
         break;
     } else if (guess.length !== 1) {
